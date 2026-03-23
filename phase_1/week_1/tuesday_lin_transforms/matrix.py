4d5dd0e02484d5316da34d98b23ad77ab16cb50b
Mahmoud Shabana <mshabana5656@gmail.com>
2026-03-23 17:01:12 -0400

    tuesday exercise done

--- phase_1/week_1/tuesday_lin_transforms/matrix.py
+++ phase_1/week_1/tuesday_lin_transforms/matrix.py
@@ -23,18 +23,51 @@ class Matrix():
         else:
             raise IndexError(f"ERROR: Invalid dimensions for multiplication: Matrix dim A ({self.rows} x {self.cols}), Vector dim ({len(vector)} X 1)")
 
+    def __repr__(self):
+        return f"Matrix({self.data})"
+    
+    def shape(self):
+        return f"({self.rows}, {self.cols})"
+
+    def transpose(self):
+        new_matrix = []
+
+        for idx in range(self.cols):
+            new_matrix.append([])
+            for row in self.data:
+                new_matrix[idx].append(row[idx])
+            
+        orig_rows = self.rows
+        orig_cols = self.cols
+        return Matrix(orig_cols, orig_rows, new_matrix)
+    
+    @staticmethod
+    def identity(n):
+        identity_matrix = []
+
+        for row_idx in range(n):
+            identity_matrix.append([])
+            for col_idx in range(n):
+                if row_idx == col_idx:
+                    identity_matrix[row_idx].append(1)
+                else:
+                    identity_matrix[row_idx].append(0)
+        
+        return Matrix(n, n, identity_matrix)
 
 def test():
 
     # testing the matrix class definition first
     mat_a = Matrix(3, 4, [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 
+    print("\n", 5*"-", "Matrix Class Definition", 5*"-")
     print(f"Definition of Matrix A:")
     print(f"Matrix A rows: {mat_a.rows}")
     print(f"Matrix A columns: {mat_a.cols}")
     print(f"Matrix A data: {mat_a.data}")
     print(f"Matrix A element at (1, 2): {mat_a.data[1][2]}")
 
+    print("\n", 5*"-", "Matrix-Vector Multiplication", 5*"-")
     vec_c = [1, 2]
     mat_b = Matrix(2, 2, [[1, 2], [3, 4]])
     print(f"Multiplication of Matrix A and Vector B:")
@@ -42,7 +75,18 @@ def test():
     print(f"Vector C: {vec_c}")
     print(f"Product of B x C: {mat_b * vec_c}")
 
+    print("\n", 5*"-", "Matrix Transpose", 5*"-")
+    mat_c = Matrix(3, 4, [[2, 4, 6, 8], [10, 12, 14, 16], [18, 20, 22, 24]])
+    mat_c_t = mat_c.transpose()
+    print(f"Matrix transposition of Matrix C of shape {mat_c.shape()}: {mat_c}")
+    print(f"Transposed Matrix C of shape {mat_c_t.shape()}: {mat_c_t}")
+
+    print("\n", 5*"-", "Identity Matrix", 5*"-")
+    identity_mat = Matrix.identity(4)
+    print(f"Defined identity matrix of size ({identity_mat.rows}, {identity_mat.cols}): {identity_mat}")
+
+
 if __name__ == "__main__":
     print("---------- Running Matrix tests ----------")
     test()
-    print("---------- Completed Matrix tests ----------")
+    print("\n---------- Completed Matrix tests ----------")
