22328957e2792bf277e38b63f4ef79c46660a58a
Mahmoud Shabana <mshabana5656@gmail.com>
2026-06-17 16:08:08 -0400

    updated script with classification and computation of vec x

--- phase_1_math/study_journal.md
+++ phase_1_math/study_journal.md
@@ -24,3 +24,11 @@ Question: When does a matrix have no inverse? What happens geometrically?
 Answer: A matrix has no inverse when it's determinant is zero. This happens because when a matrix's determinant is zero, it is a lower dimension, so geometrically, inverting the matrix in a high-dimensional space is not feasible. An example for this would be if we try to invert a cube in a 3D space. If the determinant is non-zero than the cube, regardless of any linear transformation, can be inverted along the $\hat{i}, \hat{j}, \hat{k}$ axes. However, if the determinant is zero, than the cube could be a plane in a 3D space, which is not invertible as there is a lose of information.
 
 What I mean by *loss of information* is that there are multiple distinct points of the original cube that gets mapped to the exact same location when it is flattened to a 2D plane.
+
+### Tuesday 6/16
+
+This was a touch one to implement!
+
+Question: How does elimination relate to row reduction you see in textbooks?
+
+Answer: This is basically algebraic elimination but when doing row reduction, we just extract the coefficients only and work with that.
--- phase_1_math/week_2/d2_nonsquare_dot.py
+++ phase_1_math/week_2/d2_nonsquare_dot.py
@@ -17,43 +17,81 @@ def row_add(A, i, j, c):
     temp[i] = A[i] - (c * A[j])
     return temp
 
-A = np.array([[2, 1, 1], [-3, -1, 2], [-2, 1, 2]]).astype(float)
-b = np.array([8, -11, -3]).astype(float)
+def solve(A, b):
+    if len(A) != len(b):
+        raise ValueError(f"A and b are not of equal length!: A = {A.shape}, b = {b.shape}")
+    combined_matrix = np.column_stack((A, b.T))
 
-combined_matrix = np.column_stack((A, b.T))
-print(combined_matrix)
+    for k in range(len(combined_matrix)):
+        # k will be our pivot value in an NxN matrix.
+        # Limiting k to the range of N reduces the need to avoid the "b" column
 
-# Quick func test
-swapped_matrix = row_swap(combined_matrix, 0, 1)
-mul_matrix = row_mul(combined_matrix, 0, 10)
-added_matrix = row_add(combined_matrix, 0, 1, 2)
+        # need to implement swap condition:
+        # swap if current pivot = 0.0 with any row below current pivot
+        if combined_matrix[k][k] == 0.0 and k < len(combined_matrix) - 1:
+            print("need to swap")
+            search_idx = k + 1
+            while combined_matrix[search_idx][k] == 0.0:
+                search_idx += 1
+                
+            combined_matrix = row_swap(combined_matrix, k, search_idx)
 
-# print(swapped_matrix)
-# print(mul_matrix)
-# print(added_matrix)
-
-for k in range(len(combined_matrix)):
-    # k will be our pivot value in an NxN matrix.
-    # Limiting k to the range of N reduces the need to avoid the "b" column
-    print(f"Current k: {k}")
-
-    # need to implement swap condition:
-    # swap if current pivot = 0.0 with any row below current pivot
-    if combined_matrix[k][k] == 0.0:
-        search_idx = k
-        while combined_matrix[k][search_idx] != 0.0:
-            search_idx += 1
-            if search_idx == len(combined_matrix):
-                raise ValueError("No solution for Gaussian Elimination")
-        combined_matrix = row_swap(combined_matrix, k, search_idx)
-
-    for i in range(k+1, len(combined_matrix)):
-        print(f"Current i: {i}")
-        constant = (combined_matrix[i][k]) / combined_matrix[k][k]
-        combined_matrix = row_add(combined_matrix, i, k, constant)
-        print(combined_matrix)
+        for i in range(k+1, len(combined_matrix)):
+            constant = (combined_matrix[i][k]) / combined_matrix[k][k]
+            combined_matrix = row_add(combined_matrix, i, k, constant)
+    
+    # we still need to solve for vector x...
     print(combined_matrix)
+    num_of_rows = len(combined_matrix)
+    num_of_columns = num_of_rows # we are assuming square matrix
 
+    z_c = combined_matrix[num_of_rows - 1][-2] # solution for z
+    inf_sol_check = combined_matrix[-1][-1] # check value of last element of b
+    if z_c == 0.0 and inf_sol_check != 0.0:
+        print("No Solution for this system of equations!")
+    elif z_c == 0 and inf_sol_check == 0.0:
+        print("Infinite solutions for this system of equations!")
+    else:
+        # TODO: compute vector x 
+        # this is a hardcoded solution but its fine for now
+        z = inf_sol_check / z_c
+        y_c = combined_matrix[-2][-3]
+        y = (combined_matrix[-2][-1] - (z * combined_matrix[-2][-2])) / y_c
+        x_c = combined_matrix[0][0]
+        x = (combined_matrix[0][-1] - (y * combined_matrix[0][1]) - (z * combined_matrix[0][2])) / x_c
 
-        
+        vector_x = np.array([x, y, z])
+        print(f"Unique Solution: x = {vector_x}")
+
+def test():
+    A = np.array([[2, 1, 1], [-3, -1, 2], [-2, 1, 2]]).astype(float)
+    b = np.array([8, -11, -3]).astype(float)
+
+    print("="*11, "Test 1", "="*11)
+    solve(A, b)
+    print("="*30)
+    
+    A = np.array([[1, 1, 1], [0, 2, 5], [2, 5, -1]]).astype(float)
+    b = np.array([6, -4, 27]).astype(float)
+
+    print("="*11, "Test 2", "="*11)
+    solve(A, b)
+    print("="*30)
+    
+    A = np.array([[1, 1, 1], [2, 2, 2], [1, 2, 3]]).astype(float)
+    b = np.array([1, 3, 4]).astype(float)
+
+    print("="*11, "Test 3", "="*11)
+    solve(A, b)
+    print("="*30)
+    
+    A = np.array([[1, 1, 1], [1, 2, 3], [2, 3, 4]]).astype(float)
+    b = np.array([6, 14, 20]).astype(float)
+
+    print("="*11, "Test 4", "="*11)
+    solve(A, b)
+    print("="*30)
+            
+if __name__ == "__main__":
+    test()
 
