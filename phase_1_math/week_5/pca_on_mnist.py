9e5cf1eeaa6b096ebcf3bf623f1f7abf881c4312
Mahmoud Shabana <mshabana5656@gmail.com>
2026-07-10 12:33:12 -0400

    W5D2: Updated numpy eigen func to be eigh so it doesn't return type complex values. Also plotted PCs and made sure target values are not of type str. Finally, used argsort instead of sort to get sorted idxs, not values.

--- phase_1_math/week_5/pca_on_mnist.py
+++ phase_1_math/week_5/pca_on_mnist.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from sklearn.datasets import fetch_openml
+import matplotlib.pyplot as plt
 
 
 def import_mnist():
@@ -29,13 +30,15 @@ def custom_pca(X, num_components=2):
     C = B.T @ B
 
     # get eigenvalues and eigenvectors
-    eigenvalues, eigenvectors = np.linalg.eig(C)
+    eigenvalues, eigenvectors = np.linalg.eigh(C) # switching eigh since it handle real value returns
 
     # sort eigenvals and eigenvecs in descending order
-    desc_idx = np.sort(eigenvalues)[::-1]
+    desc_idx = np.argsort(eigenvalues)[::-1] # need argsort not sort here, also eigh returns values in ascending already
     eigenvecs = eigenvectors.T # eigenvecs are returned as columns here
     sorted_eigenvals = np.array([eigenvalues[idx] for idx in desc_idx])
     sorted_eigenvecs = np.array([eigenvecs[idx] for idx in desc_idx])
+
+    print(f"First few eigenvalues: {sorted_eigenvals[:10]}")
     V = sorted_eigenvecs.T
 
     D = np.diag(sorted_eigenvals)
@@ -44,6 +47,13 @@ def custom_pca(X, num_components=2):
     T = B @ V
     return T[:, :num_components]
 
+def plot_pca(PC, y):
+    plt.scatter(PC[:, 0], PC[:, 1], s = 2, c = y.astype(int), cmap="tab10", alpha=0.4) # need to map int target vals to colors
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.title("Two Principal Components of MNIST")
+    plt.colorbar() # so we can tell which label is which color
+    plt.savefig("mnist_pca.png")
 
 def test():
     # importing dataset here so we only need to do it once
@@ -53,6 +63,7 @@ def test():
     principal_components = custom_pca(X)
 
     # TODO: now we can plot the PCs
+    plot_pca(principal_components, y)
 
 
 
