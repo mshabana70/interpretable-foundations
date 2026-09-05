3b708860f3c9afcc3d13b041092ddb16f0f51e76
Mahmoud Shabana <mshabana5656@gmail.com>
2026-09-05 13:02:52 -0400

    W6D4: working on gaussian class

--- phase_1_math/week_6/d4_EM_gaussian_mix.py
+++ phase_1_math/week_6/d4_EM_gaussian_mix.py
@@ -1,7 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+rng = np.random.default_rng(0)
 
+class Gaussian():
+
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def pdf(self, data):
+        return (1 / np.sqrt(2 * np.pi * self.std ** 2)) * np.exp((- (data - self.mean) ** 2) / (2 * (self.std ** 2)))
+
+    
 
 if __name__ == "__main__":
     print(f"Need to write EM algo for 2-component gaussian mixture model from scratch.")
