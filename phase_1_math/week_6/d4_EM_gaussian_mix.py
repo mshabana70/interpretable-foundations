d61e371fcd972e703c5fecd7679bc50fdcfb9204
Mahmoud Shabana <mshabana5656@gmail.com>
2026-09-06 09:09:10 -0400

    W6D4: tackling the EM algo again

--- phase_1_math/week_6/d4_EM_gaussian_mix.py
+++ phase_1_math/week_6/d4_EM_gaussian_mix.py
@@ -12,7 +12,19 @@ class Gaussian():
     def pdf(self, data):
         return (1 / np.sqrt(2 * np.pi * self.std ** 2)) * np.exp((- (data - self.mean) ** 2) / (2 * (self.std ** 2)))
 
-    
+    def log_pdf(self, data):
+        return (-0.5 * np.log(2 * np.pi)) - (0.5 * np.log(self.std ** 2)) - (((data - self.mean) ** 2) / (2 * self.std ** 2))
+
+    def sample(self, num_samples):
+        return rng.normal(self.mean, self.std, size=num_samples)
+
+    def fit(self, x):
+        mle_mean = np.mean(x)
+        mle_std = np.std(x)
+        return mle_mean - mle_std
+
+# TODO: Need to implement the EM algorithm for a 2-component gaussian mixture model and test on synthetic data with two clusters.
+
 
 if __name__ == "__main__":
     print(f"Need to write EM algo for 2-component gaussian mixture model from scratch.")
