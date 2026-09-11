PH2W1: Added the numerical grad func and its test funcs; also fixed the return type and shape of the loss func to make it a bit cleaner

phase_2_ml/week_1/linreg.py
@@ -26,11 +26,37 @@ def loss(X, y, w):
     n = int(X.shape[0]) # num of rows
     exp_t = predict(X, w) - y 
     loss = (1 / (2 * n)) * (exp_t.T @ (predict(X, w) - y))
-    return loss # output shape should be 1 x 1 vector
+    return loss.item() # output shape should be 1 x 1 vector, adding .item() just returns it as a scalar
 
 
 def gradient(X, y, w):
     # our derivation shows how to compute the gradient of the loss w.r.t w
     n = int(X.shape[0])
     return (1 / n) * (X.T @ (predict(X, w) - y)) # output shape should be d x 1
+
+def numerical_gradient(X, y, w, h=1e-5):
+    # what if we don't have the gradient pre-computed??
+    # we can use the central finite-difference method to do this.
+    # we already know the loss function, and we know the value we are trying to compute the gradient on (the weight vec w),
+    # so we just need to pick a value for our small step in either direction of point w => h.
+    # we also need a vec e_j so that we only step some h*e_j amount for component w_j.
+    grad_vec = np.zeros(w.shape)
+    d = w.shape[0]
+    e =  np.zeros(w.shape)
+    for j in range(d):
+        e[j] = 1.0 # set the index to 1 for the component we want to compute the grad on
+        w_plus = w.copy() + (h * e)
+        w_minus = w.copy() - (h * e)
+
+        plus_loss = loss(X, y, w_plus)
+        minus_loss = loss(X, y, w_minus)
+
+        grad_val = (plus_loss - minus_loss) / (2 * h)
+
+        grad_vec[j] = grad_val
+
+        # reset our e vec for next iter
+        e[j] = 0.0
+
+    return grad_vec
 

phase_2_ml/week_1/test_linreg.py
@@ -1,4 +1,4 @@
-from linreg import predict, loss, gradient
+from linreg import predict, loss, gradient, numerical_gradient
 import numpy as np
 import pytest
 
@@ -19,12 +19,17 @@ def test_predict_shape(create_vars):
 def test_loss_shape(create_vars):
     X, y, w = create_vars
     loss_val = loss(X, y, w) 
-    assert len(loss_val) == 1
+    assert isinstance(loss_val, float)
 
 def test_gradient_shape(create_vars):
     X, y, w = create_vars
     grad = gradient(X, y, w)
     assert grad.shape == w.shape
+
+def test_numerical_gradient_shape(create_vars):
+    X, y, w = create_vars
+    num_grad = numerical_gradient(X, y, w)
+    assert num_grad.shape == w.shape
  
 def test_loss_calculation(create_vars):
     X, y, w = create_vars
@@ -41,3 +46,10 @@ def test_loss_manual():
     loss_by_hand = np.array([[1146.5]]) # on scratch paper
     func_loss = loss(X, y, w)
     np.testing.assert_allclose(loss_by_hand, func_loss)
+
+def test_numerical_gradient_calculation(create_vars):
+    X, y, w = create_vars
+
+    predef_grad = gradient(X, y, w)
+    num_grad = numerical_gradient(X, y, w)
+    np.testing.assert_allclose(predef_grad, num_grad)