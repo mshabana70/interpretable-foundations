22fc7ac9addc2a8785e8e90aadd0d0a3cd3e2e22
Mahmoud Shabana <mshabana5656@gmail.com>
2026-09-10 22:07:19 -0400

    PH2W1: fixed the funcs so they call each other; added another loss test case that was done by hand.

--- phase_2_ml/week_1/linreg.py
+++ phase_2_ml/week_1/linreg.py
@@ -1,5 +1,4 @@
 import numpy as np
-import json
 
 
 # TODO: going to first start with the three core funcs
@@ -19,19 +18,19 @@ def predict(X, w):
     Since there is no bias value yet, y_hat = X * w
     """
 
-    return np.dot(X, w) # output shape should be n x 1 vector, matching shape of y
+    return X @ w # output shape should be n x 1 vector, matching shape of y
 
 def loss(X, y, w):
     # From our previous derivation, the loss:
     # L(w) = (1 / 2n)(Xw - y)^{T}(Xw - y)
     n = int(X.shape[0]) # num of rows
-    exp_t = np.dot(X, w) - y 
-    loss = (1 / 2) * n * (exp_t.T @ (np.dot(X, w) - y))
-    return loss # output shape should be 1 x 1 scalar
+    exp_t = predict(X, w) - y 
+    loss = (1 / (2 * n)) * (exp_t.T @ (predict(X, w) - y))
+    return loss # output shape should be 1 x 1 vector
 
 
 def gradient(X, y, w):
     # our derivation shows how to compute the gradient of the loss w.r.t w
     n = int(X.shape[0])
-    return (1 / n) * (X.T @ (np.dot(X, w) - y)) # output shape should be d x 1
+    return (1 / n) * (X.T @ (predict(X, w) - y)) # output shape should be d x 1
 
--- phase_2_ml/week_1/test_linreg.py
+++ phase_2_ml/week_1/test_linreg.py
@@ -2,7 +2,7 @@ from linreg import predict, loss, gradient
 import numpy as np
 import pytest
 
-rng = np.random.default_rng()
+rng = np.random.default_rng(seed=42)
 
 @pytest.fixture
 def create_vars(n=3, d=2):
@@ -26,3 +26,18 @@ def test_gradient_shape(create_vars):
     grad = gradient(X, y, w)
     assert grad.shape == w.shape
  
+def test_loss_calculation(create_vars):
+    X, y, w = create_vars
+    n = int(X.shape[0])
+    man_loss = (1 / (2 * n)) * (((X @ w) - y).T @ ((X @ w) - y))
+    func_loss = loss(X, y, w)
+    np.testing.assert_allclose(man_loss, func_loss) # have to use this np assert func because of floating point precision comparison; good to remember for the future
+
+def test_loss_manual():
+    X = np.array([[1, 5], [9, 12]])
+    w = np.array([[2], [4]])
+    y = np.array([[3], [1]])
+
+    loss_by_hand = np.array([[1146.5]]) # on scratch paper
+    func_loss = loss(X, y, w)
+    np.testing.assert_allclose(loss_by_hand, func_loss)
